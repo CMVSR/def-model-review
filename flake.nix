0fcc984dfbb4a01f809a5e2a20d1aab1ad15f457
Noah Gardner <ngngardner@gmail.com>
2021-12-20 10:21:14 -0500

    add initial documents

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "NLP Project";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/d189bf92f9be23f9b0f6c444f6ae29351bb7125c";
+    utils = { url = "github:numtide/flake-utils"; };
+  };
+
+  outputs = { self, nixpkgs, utils }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+      {
+        devShell = pkgs.mkShell {
+          packages = [
+            # tex
+            (pkgs.texlive.combine {
+              inherit (pkgs.texlive)
+                scheme-small
+                latexmk
+                latexindent
+
+                biblatex
+                ;
+            })
+          ];
+        };
+      });
+}
